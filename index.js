a18a57a18855b4f55a20c35641a1b1825980dd22
Christian Liebel <christian.liebel@thinktecture.com>
2018-06-08 09:36:15 +0200

    feat(api): add CORS support

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const logger = require('restify-logger');
 const server = restify.createServer();
 const jwt = require('restify-jwt-community');
 const jwks = require('jwks-rsa');
+const corsMiddleware = require('restify-cors-middleware');
+
+const cors = corsMiddleware({
+    origins: ['*'],
+});
+
+server.pre(cors.preflight);
+server.use(cors.actual);
 
 server.use(restify.plugins.bodyParser());
 server.use(logger('combined'));
